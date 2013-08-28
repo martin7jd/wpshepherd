f381b66ecf970f7ad3a7f49f6009fbf0c3a00d79
Martin  Rose <martin7jd@yahoo.co.uk>
2013-08-28 16:04:47 +0100

    Removed con_localhost.php

--- index_inst.php
+++ index_inst.php
@@ -1,28 +1,11 @@
 <?php
 
-# This page is created when the user adds the MySql user name and password 
-  # Add the values to id="user" and id="pass".
+    echo '<div id="sitename">';     
 
-  # Form to enter the website name that is to be used  
+		echo '<h3>Warning</h3>';
   
-  # This page gathers the information together to install a version of wordpress
-  # as a localhost copy.
-  # Admin_main is set-up first so that the installation of each copy can take place.
-  # It follows the "Famous 5-Minute Install"
-  
-  # See if there are any credentials in the wpadmin_localhost.localhost_info .
-  # If not direct the user to the "Admin" tab and the "Installation settings" link
-
-
-	echo '<div id="sitename">';  # When the "Submit" button is press it launches install_one.php
-
-	echo '<h3>Web Site Name<h3/>';
-
-	echo '<input id="web" type="text" name="web" placeholder="Type Name Here" required><br/><br/>';
-
-
-	echo '<button type="button" onclick="inst_one()">Fresh Install!</button>';
+  		# Splash screen when there are no credentials. They need setting up
+		  echo 'You need to click on the Admin tab and select Installation Settings';
 
 	echo '</div>';
-
 ?>
